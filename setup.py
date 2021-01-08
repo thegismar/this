0f3c6ba37e571b9129b6682ee67db8ae0bc3499e
Gismar <thegismar@gmail.com>
2021-01-08 10:06:26 -0300

    x

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="py-this", # Replace with your own username
-    version="0.0.3",
+    version="0.0.4",
     packages=setuptools.find_packages()
     ,
     python_requires='>=3.9',
