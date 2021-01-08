f93f0b3999b84cabd92da189d5f1f773fd80973f
Gismar <thegismar@gmail.com>
2021-01-08 07:45:12 -0300

    changed name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="py-this", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     packages=setuptools.find_packages()
     ,
     python_requires='>=3.9',
